2632e695e7d51475f6c58bd051478e4f751ba3a1
omar-raspberry-pi <80557610+omar-raspberry-pi@users.noreply.github.com>
2022-05-08 19:20:20 +0300

    Add files via upload

---
+++ o.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="الاصدار التجربي"
+TITLE="واجهة جنوم"
+MENU="اختر"
+
+OPTIONS=(1 "إنشاء مستند نصي جديد"
+2 "إنشاء  مسند شل "
+         3 "خروج"
+)
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            
+            gedit newtext.txt
+            ;;
+        2)
+            echo "You chose Option 2"
+            gedit newtext.sh
+            ;;
+        3)
+            echo "You chose Option 3"
+            exit
+            ;;
+            
+        4)
+esac
